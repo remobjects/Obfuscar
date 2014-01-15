3146fee0333c0a3f79a2d78e45528ce9f09f3727
Carlo Kok <ck@remobjects.com>
2014-01-15 13:18:42 +0100

    0: More history rewriting.

---
+++ Obfuscar/Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle("Obfuscar Console Utility")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
